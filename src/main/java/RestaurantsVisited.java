d43fbf7bfe6c1d9dd256b34476555c629af5fba2
chitra atmakuru & andrea padgett <student@epicodus.com>
2015-08-18 12:19:15 -0700

    app working

--- src/main/java/RestaurantsVisited.java
+++ src/main/java/RestaurantsVisited.java
@@ -6,18 +6,47 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 import java.util.Random;
-import java.util.*;
-import java.text.*;
+import java.util.ArrayList;
+// import java.text.*;
 
 public class RestaurantsVisited {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("restaurantsFinalList", request.session().attribute("sessionRest"));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/restaurant", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      ArrayList<RestaurantsVisited> restaurantsArray = request.session().attribute("sessionRest");
+
+      if ( restaurantsArray == null) {
+        restaurantsArray = new ArrayList<RestaurantsVisited>();
+        request.session().attribute("sessionRest", restaurantsArray);
+      }
+
+      String name = request.queryParams("name");
+      String dateVisited = request.queryParams("dateVisited");
+      RestaurantsVisited newRestaurant = new RestaurantsVisited(name, dateVisited);
+      restaurantsArray.add(newRestaurant);
+      //System.out.println(dateVisited);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
 
   }
 
   private String mName;
-  private SimpleDateFormat mVisited;
+  private String mVisited;
 
-  public RestaurantsVisited( String name, SimpleDateFormat visited) {
+  public RestaurantsVisited( String name, String visited) {
     mName = name;
     mVisited = visited;
   }
@@ -27,9 +56,10 @@ public class RestaurantsVisited {
   }
 
   public String getDate(){
-    Date date = new Date();
-    String stringVisited = mVisited.format(date);
-    return stringVisited;
+  //  Date date = new Date();
+  //  String stringVisited = mVisited.format(date);
+    //return stringVisited;
+    return mVisited;
   }
 
 }
--- src/test/java/RestaurantsVisitedUnitTest.java
+++ src/test/java/RestaurantsVisitedUnitTest.java
@@ -1,29 +1,51 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.util.*;
-import java.text.*;
+// import java.util.*;
+// import java.text.*;
 
 public class RestaurantsVisitedUnitTest {
 
+  // @Test
+  // public void restaurantsVisited_instantiatesCorrectly_true() {
+  //   SimpleDateFormat date = new SimpleDateFormat("2012-12-05");
+  //   RestaurantsVisited myRestaurants = new RestaurantsVisited("McDonalds", date);
+  //   assertEquals(true, myRestaurants instanceof RestaurantsVisited);
+  // }
+  //
+  // @Test
+  // public void restaurantsVisited_instantiatsWithDescriptionName_true() {
+  //   SimpleDateFormat date = new SimpleDateFormat("2012-12-05");
+  //   RestaurantsVisited myRestaurants = new RestaurantsVisited("McDonalds", date);
+  //   assertEquals("McDonalds", myRestaurants.getName());
+  // }
+  //
+  // @Test
+  // public void restaurantsVisited_instantiatsWithDescriptionDate_true() {
+  //   SimpleDateFormat date = new SimpleDateFormat("2012-12-05");
+  //   RestaurantsVisited myRestaurants = new RestaurantsVisited("McDonalds", date);
+  //   assertEquals("2012-12-05", myRestaurants.getDate());
+  // }
+  //
+
+
   @Test
   public void restaurantsVisited_instantiatesCorrectly_true() {
-    SimpleDateFormat date = new SimpleDateFormat("2012-12-05");
+    String date = new String ("2012-12-05");
     RestaurantsVisited myRestaurants = new RestaurantsVisited("McDonalds", date);
     assertEquals(true, myRestaurants instanceof RestaurantsVisited);
   }
 
   @Test
   public void restaurantsVisited_instantiatsWithDescriptionName_true() {
-    SimpleDateFormat date = new SimpleDateFormat("2012-12-05");
+    String date = new String ("2012-12-05");
     RestaurantsVisited myRestaurants = new RestaurantsVisited("McDonalds", date);
     assertEquals("McDonalds", myRestaurants.getName());
   }
 
   @Test
   public void restaurantsVisited_instantiatsWithDescriptionDate_true() {
-    SimpleDateFormat date = new SimpleDateFormat("2012-12-05");
+    String date = new String ("2012-12-05");
     RestaurantsVisited myRestaurants = new RestaurantsVisited("McDonalds", date);
     assertEquals("2012-12-05", myRestaurants.getDate());
   }
-
 }
